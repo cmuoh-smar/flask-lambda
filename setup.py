e50a69bbff69bc9330876f9b2b3a52c8e988f777
Andrew Griffiths <andrewgriffithsonline@protonmail.com>
2023-06-27 15:15:00 +0100

    bumps version to v0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.rst') as f:
 
 setup(
     name='flask-lambda-python36',
-    version='0.1.0',
+    version='0.1.1',
     description=('Python3.6+ module to make Flask compatible with AWS Lambda'),
     long_description=long_description,
     keywords='flask aws amazon lambda',
